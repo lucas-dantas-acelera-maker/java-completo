e123f392ad7f3e4b4e214843ad907eb45cfee94c
lucas-dantas-acelera-maker <aluno17@aceleramaker.com.br>
2025-03-24 15:04:49 -0300

    feat: add String related functions

---
+++ src/s09streamapi/challenge/utils/StringOps.java
@@ -0,0 +1,14 @@
+package s09streamapi.challenge.utils;
+
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
+
+public class StringOps {
+
+    private StringOps() {
+    }
+
+    public static final Function<Integer, String> convertIntToBinaryStr = Integer::toBinaryString;
+    public static final UnaryOperator<String> reverseString = s -> new StringBuffer(s).reverse().toString();
+    public static final Function<String, Integer> convertBinaryStrToInt = s -> Integer.parseInt(s, 2);
+}
